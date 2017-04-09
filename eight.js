aa5aeb6f66bcdc2a37834e17545c878e1009435e
rishona <rishona>
2017-04-08 23:09:43 -0400

    88888888

---
+++ eight.js
@@ -0,0 +1,28 @@
+$(document).ready(main);
+
+function main()
+{
+    var eight=$("#eight video");
+
+    eight.click(eighteighteight);
+}
+
+function eight()
+{
+    var eight=Math.random()*(89-1)+1;
+    
+    var eighteight="";
+
+    for (x=0;x<eight;x++)
+    {
+        eighteight+="8";
+    }
+
+    return eighteight;
+}
+
+function eighteighteight()
+{
+    this.play();
+    console.log(eight());
+}
